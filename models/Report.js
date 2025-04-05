45b5162f4ec76918f94ba6e088cfeac37e3134d5
Divyata Pandey <101250441+divyatapandey@users.noreply.github.com>
2025-04-05 21:19:46 +0530

    Update Report.js
    added email

--- models/Report.js
+++ models/Report.js
@@ -4,8 +4,8 @@ const reportSchema = new mongoose.Schema({
     filename: { type: String, required: true },
     extractedText: { type: String, required: true },
     analysis: { type: Object, required: true },
+    email: { type: String, required: true },  
 });
 
 const Report = mongoose.model('Report', reportSchema);
-
 module.exports = { Report };
