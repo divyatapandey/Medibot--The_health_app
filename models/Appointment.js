93d0abb79fb127e33ca2e673bf3b062f904773f8
Divyata Pandey <101250441+divyatapandey@users.noreply.github.com>
2025-04-07 15:29:59 +0530

    Update Appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const AppointmentSchema = new mongoose.Schema({
+     email: {
+        type: String, 
+        required: true 
+    },
     doctorName: { 
         type: String, 
         required: true 
